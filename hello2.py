5ac9b6f37df4f41959b7982f1e39a6bfb19c0d46
ChandraPrakash1486 <chandraprakashtekwani8@gmail.com>
2023-11-13 12:54:06 +0530

    first commit

---
+++ hello2.py
@@ -0,0 +1,15 @@
+def a():
+    list1  = []
+    list2 = []
+    x   = int(input("Enter The Number: "))
+    y = int(input("Enter the "))
+    for i in range(y):
+        if (i%2==0):
+            list1.append(i)
+        else:
+            list2.append(i)
+    
+
+    print("Even Numbers: ",list1)
+    print("Odd Numbers: ",list2)
+a()
