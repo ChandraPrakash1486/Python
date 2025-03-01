e329ea8d5d829552cbbae3b5f4eb88cd58c8214b
ChandraPrakash1486 <chandraprakashtekwani8@gmail.com>
2025-03-01 21:09:16 +0530

    Created New Folder named-Multithreading, Multiprocessing, async_await and Function Programming, and wrote code of Functional Programming.

--- git_commands.sh
+++ git_commands.sh
@@ -3,3 +3,4 @@ git add .
 git commit -m "Created New Folder named-Multithreading, Multiprocessing, async_await and Function Programming, and wrote code of Functional Programming."
 git remote add origin https://github.com/ChandraPrakash1486/Python.git
 git push -u origin main
+git remote remove origin
